0cd5ca5ee1bf8425c2510f7aafac11c9bcaa40e1
Eduardo Ferro Aldama <eduardo.ferro.aldama@gmail.com>
2015-08-22 09:59:24 +0200

    examples

--- t1.py
+++ t1.py
@@ -1,15 +1,21 @@
 from pydatastructsalgorithms import tree
 
 r = tree.binary_tree(3)
-print r
 tree.insert_left(r, 4)
-print r
 tree.insert_left(r, 5)
-print r
-
 tree.insert_right(r, 6)
-print r
 tree.insert_right(r, 7)
-print r
+
+l = tree.get_left_child(r)
+print l
 
 print "---"
+tree.set_root_val(l, 9)
+print l
+tree.insert_left(l, 11)
+print l
+
+
+print "---"
+print r
+print(tree.get_right_child(tree.get_right_child(r)))
